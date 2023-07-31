bf0d648374e5cbece0a5aa0c4fdf0faed32e7c12
exponentian <exponentian.oh@gmail.com>
2023-07-31 14:36:28 -0700

    updated

--- SoilX/global.js
+++ SoilX/global.js
@@ -3,7 +3,7 @@
   m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
   })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-  ga('create', 'UA-17301459-14', 'soilx.ca');
+  ga('create', 'ID', 'soilx.ca');
   ga('send', 'pageview');
   
   
